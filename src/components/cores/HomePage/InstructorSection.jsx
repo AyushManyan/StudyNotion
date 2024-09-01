6b9e4c2b596ed28bdcdc19afe7c92a4a55b9498d
Ayush <ayushmanyan2003@gamil.com>
2024-09-02 00:55:05 +0530

    adding InstructorSection, ReviewSlider, LearningLanguageSection

---
+++ src/components/cores/HomePage/InstructorSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const InstructorSection = () => {
+  return (
+    <div>InstructorSection</div>
+  )
+}
+
+export default InstructorSection
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,9 @@ import Banner from "../assets/Images/banner.mp4";
 import CodeBlocks from '../components/cores/HomePage/CodeBlocks';
 import TimelineSection from '../components/cores/HomePage/TimelineSection';
 import LearningLanguageSection from '../components/cores/HomePage/LearningLanguageSection';
+import InstructorSection from "../components/cores/HomePage/InstructorSection"
+import ReviewSlider from "../components/cores/HomePage/ReviewSlider"
+
 
 function Home() {
     return (
@@ -134,11 +137,12 @@ function Home() {
 
             <div className='bg-pure-greys-5 text-richblack-700'>
                 <div className='homepage_bg h-[320px]'>
-                    <div className='w-11/12 max-w-maxContent flex flex-col items-center justify-center gap-5 mx-auto'>
+                    {/* Explore Full Catagory Section */}
+                    <div className='mx-auto flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8'>
                         <div className='lg:h-[150px]'></div>
-                        <div className='flex flex-row gap-7 text-white'>
+                        <div className='flex flex-row gap-7 text-white lg:mt-8'>
                             <CTAButton active={true} linkto={"/signup"}>
-                                <div className='flex items-center gap-3'>
+                                <div className='flex items-center gap-2'>
                                     Explore Full CAtalog
                                     <FaArrowRight></FaArrowRight>
                                 </div>
@@ -155,17 +159,18 @@ function Home() {
                 </div>
 
 
-                <div className='mx-auto w-11/12 max-w-maxContent flex flex-col justify-between items-center gap-5'>
+                <div className='mx-auto flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8 '>
 
-                    <div className='flex flex-row gap-5 mb-10 mt-[95px]'>
+                        {/* Job that is in Demand - Section 1 */}
+                    <div className='mb-10 mt-[-100px] flex flex-col justify-between gap-7 lg:mt-20 lg:flex-row lg:gap-0'>
 
-                        <div className='text-4xl font-semibold w-[45%]'>
+                        <div className='text-4xl font-semibold lg:w-[45%]'>
                             Get the Skills you need for a
                             <HighlightText text={" Job that is in demand"} />
                         </div>
 
 
-                        <div className='flex flex-col gap-10 w-[40%] items-start'>
+                        <div className='flex flex-col items-start gap-10 lg:w-[40%]'>
                             <div className='text-[16px]'>
                                 The modern StudyNotion is the dictates its own terms. Today, to
                                 be a competitive specialist requires more than professional
@@ -180,12 +185,12 @@ function Home() {
 
                         </div>
 
-
-
                     </div>
 
+                        {/* Timeline Section - Section 2 */}
                     <TimelineSection />
 
+                        {/* Learning Language Section - Section 3 */}
                     <LearningLanguageSection />
 
 
@@ -196,6 +201,16 @@ function Home() {
 
 
             {/* section 3 */}
+
+            <div className='relative mx-auto my-20 flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8 bg-richblack-900 text-white'>
+
+            <InstructorSection/>
+
+            <h2 className="text-center text-4xl font-semibold mt-8">Review from other Learners</h2>
+
+            </div>
+
+            <ReviewSlider/>
 
 
             {/* footer */}
