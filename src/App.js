af57fdd5b3cabba113f29a7a9abe445a325c924e
Ayush <ayushmanyan2003@gamil.com>
2024-09-07 02:21:37 +0530

    adding hooks, signup, login form, profiledropdown page, connecting backend

--- src/App.js
+++ src/App.js
@@ -1,14 +1,54 @@
 // import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/common/Navbar"
+import OpenRoute from "./components/cores/Auth/OpenRoute"
+
+// {pages}
 import Home from "./pages/Home"
-function App(){
-  return(
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
+import VerifyEmail from "./pages/VerifyEmail"
+
+
+
+
+function App() {
+  return (
     <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        
-        </Routes>
+
+        {/* Open Route - for Only Non Logged in User */}
+        <Route
+          path="login"
+          element={
+            <OpenRoute>
+              <Login />
+            </OpenRoute>
+          }
+        />
+
+        <Route
+          path="signup"
+          element={
+            <OpenRoute>
+              <Signup />
+            </OpenRoute>
+          }
+        />
+
+        <Route
+          path="verify-email"
+          element={
+            <OpenRoute>
+              <VerifyEmail />
+            </OpenRoute>
+          }
+        />
+
+      </Routes>
     </div>
   )
 }
